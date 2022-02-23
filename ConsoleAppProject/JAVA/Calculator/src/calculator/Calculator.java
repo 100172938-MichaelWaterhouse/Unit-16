b7a83b659a95c52126268089963b3c3c8272bb17
Michael Waterhouse <100172938@student.burnley.ac.uk>
2022-02-23 16:03:26 +0000

    more

--- ConsoleAppProject/JAVA/Calculator/src/calculator/Calculator.java
+++ ConsoleAppProject/JAVA/Calculator/src/calculator/Calculator.java
@@ -9,13 +9,36 @@ package calculator;
  * @author 100172938
  */
 public class Calculator {
+    String name;
+    
+    public int add(int x, int y)
+    {
+        return x + y;
+    }
+    public int subtract(int x, int y)
+    {
+        return x - y;
+    }
+    public int multiply(int x, int y)
+    {
+        return x * y;
+    }
+    public int divide(int x, int y)
+    {
+        return x / y;
+    }
+    public int modulus(int x, int y)
+    {
+        return x + y;
+    }
 
     /**
      * @param args the command line arguments
      */
+    public class mainClass {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World");
+       
         
     }
     
