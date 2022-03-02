b0eaa82033a8f1f08e11830b479b55b199a5e071
Michael Waterhouse <100172938@student.burnley.ac.uk>
2022-03-02 14:04:03 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,44 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This class offers methods for converting a given
+    /// distance measured in miles to the equivalent
+    /// distance measured in feet
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Michael Waterhouse version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
 
+        private double feet; 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Primpt user to enter the distance in miles
+        /// Input the miles as a double number 
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+
+        private void CalculateFeet()
+        {
+
+        }
+
+        private void OutputFeet()
+        {
+
+        }
     }
 }
